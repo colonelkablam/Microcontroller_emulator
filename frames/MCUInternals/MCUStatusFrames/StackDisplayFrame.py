1721080e287a31911b77fca1198aff60e9c48379
Nick Harding <w1924972@westminster.ac.uk>
2023-08-17 08:33:25 +0100

    stack

--- frames/MCUInternals/MCUStatusFrames/StackDisplayFrame.py
+++ frames/MCUInternals/MCUStatusFrames/StackDisplayFrame.py
@@ -7,7 +7,7 @@ class StackDisplayFrame(ttk.Frame):
         super().__init__(parent, *args, **kwargs)
 
         # styling
-        self.configure(style="MainWindowInner2.TFrame", padding=5, width=50)
+        self.configure(style="MainWindowInner2.TFrame", padding=5)
         self.columnconfigure(0, weight=1)
         #self.rowconfigure(0, weight=1)
         self.grid(sticky="NSEW")
@@ -16,15 +16,19 @@ class StackDisplayFrame(ttk.Frame):
         self.parent = parent
         self.stack = stack
         self.stack_display_list = []
+        self.previous_length = 0
 
         # tkinter widgets
         # heading
-        self.stack_label = ttk.Label(self, text="STACK", style='MainWindowInner3.TLabel')
-        self.stack_label.grid(column=0, row=0, pady=(0,0), padx=(0, 0), sticky="W")
+        self.stack_label = ttk.Label(self, text=" STACK:\n-  top  -", style='MainWindowInner3.TLabel')
+        self.stack_label.grid(column=0, row=0, pady=(0,0), padx=(0, 0), sticky="EW")
 
-        # initialise the stack display so will show a holding label
-        # add empty 'backstop to stack'
-        self.initialise_stack_display_list("- empty -")
+        # frame for list of stack elements
+        self.list_frame = ttk.Frame(self, style='MainWindowInner3.TLabel')
+        self.list_frame.grid(column=0, row=1, pady=(0,0), padx=(0, 0), sticky="NSEW")
+
+        # initialise the stack display
+        self.initialise_stack_display_list()
 
         
     # STACK methods
@@ -32,38 +36,38 @@ class StackDisplayFrame(ttk.Frame):
     def update(self):
         print(self.stack)
         stack_length = len(self.stack)
-        # test to see if empty
-        if stack_length == 0:
-            self.stack_display_list[0].configure(text="- empty -")
-            self.stack_display_list[0].grid()
-        else:
-            i = 1
-            for element in self.stack_display_list:
-                if i <= stack_length:
-                    element.configure(text=f"{i} - {self.stack[i-1]:04X}h")
-                    element.grid()
-                else:
-                    element.grid_remove()
-                i+=1
 
-            self.stack_display_list[i+1].grid()
-            self.stack_display_list[i+1].configure(text="-  end  -")
+        for i, element in enumerate(self.stack_display_list, 1):
+            if stack_length == 0:
+                element.configure(text="  empty  ", style="Stack.TLabel")
+                element.grid()
+                break
+            elif i <= stack_length:
+                element.configure(text=f"{i} - {self.stack[i-1]:04X}h", style="Stack.TLabel" )
+                element.grid()
+            elif i == stack_length + 1:
+                element.configure(text="-bottom-", style='MainWindowInner3.TLabel')
+                element.grid()
+            else:
+                element.grid_remove()
 
 
-
-    def initialise_stack_display_list(self, text):
-        for stack_level in range(0, STACK_SIZE):
-            stack_element = ttk.Label(  self,
+    def initialise_stack_display_list(self):
+        for stack_level in range(0, STACK_SIZE + 1): # +1 to store last status label
+            stack_element = ttk.Label(  self.list_frame,
                                         width=9, 
                                         style="Stack.TLabel"      )
-            stack_element.grid(column=0, row=stack_level, padx=0, pady=(0,3), sticky="W")
+            stack_element.grid(column=0, row=stack_level, padx=0, pady=(0,3), sticky="EW")
             stack_element.grid_remove()
             self.stack_display_list.append(stack_element)
-
-            self.stack_display_list[0].configure(text=text)
-            self.stack_display_list[0].grid()
+        
+        # add empty 'backstop to stack'
+        self.stack_display_list[0].configure(text="  empty  ", style="Stack.TLabel")
+        self.stack_display_list[0].grid()
+        self.stack_display_list[1].configure(text="-  end  -", style='MainWindowInner3.TLabel')
+        self.stack_display_list[1].grid()
             
-            # add empty 'backstop to stack'
+
 
         
 
