37f8aa94a039274629a04bf8bff9c15a8cf69b89
nickh <nickhfharding@gmail.com>
2023-09-03 07:18:38 +0100

    main update

--- project_test_2.asm
+++ project_test_2.asm
@@ -1,16 +1,41 @@
-main:
+; testing CALL and RETURN
 
-ADDLW	0xA0
-MOVWF	0x20
-CALL	subroutine
+main:	PSECT	0x0000	; main location at 0x0000
+	
+	CLRF	TRISB
+	CLRF	PORTB
+	CLRF	PORTA
 
-CLRW
+	ADDLW	0x0B
+	MOVWF	PORTB
 
-GOTO	main
-
-
-subroutine:
 loop:
-DECFSZ	0x20	1
-GOTO	loop
-RETURN
+	BTFSS	0x05	0	; test pin 0, port A
+	GOTO	loop
+
+	DECF	PORTB
+	MOVF	PORTB	1	; test PORTB reached 0 
+	BTFSS	STATUS	2	; STATUS Z will be 1
+	GOTO	loop
+
+	CALL a			; go into stack test
+
+	CLRF	0x21
+
+end_loop:
+	INCF	0x21	1
+
+	GOTO	end_loop		; stay in end loop when done
+
+
+; subroutines
+a:
+	CALL b
+	RETURN
+
+b:
+	CALL c
+	RETURN
+
+c:
+	RETURN
